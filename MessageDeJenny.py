c3b36a9d1038a8fb89abe278c7828ce6fd2e08ef
aminedouibi <mea.douibi@it-students.fr>
2022-05-02 14:05:03 +0200

    rajout exercice Jenny

---
+++ MessageDeJenny.py
@@ -0,0 +1,11 @@
+def AffichePrenom (prenom1,prenom2):
+ 
+   
+    prenom1 = input("Bonjour , quel est ton prenom ?")
+    
+    if prenom1=="Johnny":
+        print ("Hello my Love")
+    else:
+        prenom2=input("mon prenom1")
+        print(f"Bonjour {prenom1} enchanté  , moi c est {prenom2}")
+AffichePrenom("","")
