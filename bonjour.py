0a044b31da2767bd28660f46e00a4e05bdeac23b
aminedouibi <mea.douibi@it-students.fr>
2022-05-02 12:32:33 +0200

    rajout correctif

--- bonjour.py
+++ bonjour.py
@@ -2,5 +2,5 @@ def AffichePrenom (prenom):
  
     #print("Bonjour",prenom,"comment allez Vous ?")
     prenom = input("entrez le prenom")
-    print(f"{prenom} comment allez Vous ?")
+    print(f"Bonjour {prenom} comment allez Vous ?")
 AffichePrenom("")
